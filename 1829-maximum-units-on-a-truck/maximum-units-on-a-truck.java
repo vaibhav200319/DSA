aa2cee1c362b9a206726c2f54949d0ca9a5f2972
VaibhavYelis19 <vaibhavyelis@gmail.com>
2026-01-03 13:20:46 +0530

    Time: 17 ms (8.31%) | Memory: 46.4 MB (96.69%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
@@ -1,20 +1,19 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         Arrays.sort(boxTypes, (x,y) -> y[1] - x[1]);
+        int unit = 0;
 
-        int units = 0;
+        for(int i=0; i<boxTypes.length && truckSize>0; i++){
 
-        for(int i=0; i<boxTypes.length && truckSize > 0; i++){
+            int BB = boxTypes[i][0];
+            int BBB = boxTypes[i][1];
 
-            int boxes = boxTypes[i][0];
-            int preBox = boxTypes[i][1];
-
-            while(boxes > 0 && truckSize > 0){
-                units += preBox;
-                boxes--;
+            while(BB>0 && truckSize>0){
+                unit += BBB;
+                BB--;
                 truckSize--;
             }
         }
-        return units;
+        return unit;
     }
 }
